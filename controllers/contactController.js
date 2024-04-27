f12b5fe3651379c48798cff14bc650a5282adccf
Tagg McDonald <taggmcd@gmail.com>
2024-04-26 19:23:06 -0600

    W01 Project

---
+++ controllers/contactController.js
@@ -0,0 +1,31 @@
+const mongodb = require('../database/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const index = async (req, res) => {
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('contacts')
+    .find({});
+
+  result.toArray().then((contacts) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contacts);
+    console.log(contacts);
+  });
+};
+
+const show = async (req, res) => {
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('contacts')
+    .find({ _id: new ObjectId(req.params.id) });
+
+  result.toArray().then((contact) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(contact);
+  });
+};
+
+module.exports = { index, show };
---
+++ server.js
@@ -0,0 +1,21 @@
+// Express
+const express = require('express');
+const app = express();
+
+// Database
+const mongodb = require('./database/mongodb.js');
+
+// Routes
+app.use('/', require('./routes'));
+
+// Connect to MongoDB and start the server
+mongodb.init((err) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log('Connected to MongoDB');
+
+  app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+  });
+});
