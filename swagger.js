6c2dd3b4b705f87052a3c9a1548bedf334108036
Tagg McDonald <taggmcd@gmail.com>
2024-05-04 10:40:47 -0600

    Adding port variable

--- swagger.js
+++ swagger.js
@@ -1,6 +1,7 @@
 const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/contacts.js'];
+const port = process.env.PORT || 3000;
 
 const doc = {
   info: {
@@ -8,7 +9,7 @@ const doc = {
     title: 'Contacts API',
     description: 'A RESTful API for your contacts.',
   },
-  host: 'localhost:3000',
+  host: `localhost:${port}`,
   basePath: '/',
   schemes: ['http', 'https'],
 };
